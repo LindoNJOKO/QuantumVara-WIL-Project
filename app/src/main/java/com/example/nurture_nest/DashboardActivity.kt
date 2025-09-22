b053f44b52affcfdbf9346869eba9bf1dc011401
ST10145657 <ST10145657@vcconnect.edu.za>
2025-09-22 21:59:31 +0200

    UI Fix

--- app/src/main/java/com/example/nurture_nest/DashboardActivity.kt
+++ app/src/main/java/com/example/nurture_nest/DashboardActivity.kt
@@ -2,8 +2,8 @@ package com.example.nurture_nest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-//import com.example.nurture_nest.Fragments.ParentDashboardFragment
-//import com.example.nurture_nest.Fragments.TeacherDashboardFragment
+import com.example.nurture_nest.Fragments.ParentDashboardFragment
+import com.example.nurture_nest.Fragments.TeacherDashboardFragment
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -13,16 +13,16 @@ class DashboardActivity : AppCompatActivity() {
 
         val role = intent.getStringExtra("userType")
 
-        //val fragment = when (role) {
-          //  "Parent" -> ParentDashboardFragment()
-            //"Teacher" -> TeacherDashboardFragment()
-            //else -> null
-        //}
+        val fragment = when (role) {
+            "Parent" -> ParentDashboardFragment()
+            "Teacher" -> TeacherDashboardFragment()
+            else -> null
+        }
 
-        //fragment?.let {
-        //    supportFragmentManager.beginTransaction()
-        //        .replace(R.id.fragment_container, it)
-        //        .commit()
-        //}
+        fragment?.let {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, it)
+                .commit()
+        }
     }
 }
